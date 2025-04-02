9b98001efe5471d25fecde29c8c24be95c6c6d51
cou <courange.c@gmail.com>
2025-04-03 03:08:51 +0900

    add led

--- keyboards/atlas_touch_v1/keymaps/default/keymap.c
+++ keyboards/atlas_touch_v1/keymaps/default/keymap.c
@@ -4,116 +4,99 @@
 #include QMK_KEYBOARD_H
 #include "os_detection.h"
 
-#define DEFAULT_LAYER 0
+enum {
+    DEFAULT_LAYER = 0,
+    CONTROL_LAYER,
+    MACHINE_CTRL_LAYER,
+    GAMING_LAYER,
+    TOTAL_LAYERS
+};
+
 #define DL DEFAULT_LAYER
-
-#define CONTROL_LAYER 1
 #define CL CONTROL_LAYER
-
-#define MACHINE_CTRL_LAYER 2
 #define ML MACHINE_CTRL_LAYER
+#define GL GAMING_LAYER
 
-#define REMOTE_DEFAULT_LAYER 3
-#define RDL REMOTE_DEFAULT_LAYER
+enum { RGBRST = SAFE_RANGE, KC_NWIN, KC_BWIN, KC_WSS, PAREN, EMAIL_1, EMAIL_2, EMAIL_3, UNI_NUM, L_RESET, KC_RNWIN, NAME, BIRTH, P_SHIFT, KC_MCWIN };
 
-#define REMOTE_CTL_LAYER 4
-#define RCL REMOTE_CTL_LAYER
+const rgblight_segment_t PROGMEM l1[]=RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_AZURE});
+const rgblight_segment_t PROGMEM l2[]=RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_BLUE});
+const rgblight_segment_t PROGMEM l3[]=RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_CHARTREUSE});
+const rgblight_segment_t PROGMEM l4[]=RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_GOLD});
 
-#define REMOTE_MACHINE_CTL_LAYER 5
-#define RML REMOTE_MACHINE_CTL_LAYER
+const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    l1, l2, l3, l4
+);
 
-#define MAC_DEFAULT_LAYER 6
-#define MDL MAC_DEFAULT_LAYER
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(0,layer_state_cmp(state,DEFAULT_LAYER));
+    rgblight_set_layer_state(1,layer_state_cmp(state,CONTROL_LAYER));
+    rgblight_set_layer_state(2,layer_state_cmp(state,MACHINE_CTRL_LAYER));
+    rgblight_set_layer_state(3,layer_state_cmp(state,GAMING_LAYER));
 
-#define MAC_CTL_LAYER 7
-#define MCL MAC_CTL_LAYER
+    return state;
+}
 
-#define MAC_MACHINE_CTL_LAYER 8
-#define MML MAC_MACHINE_CTL_LAYER
-
-#define TOTAL_LAYERS 8
-
-static bool is_apple                          = false;
-static int  is_called_keyboard_post_init_user = 0;
-
-enum custom_keycodes { RGBRST = SAFE_RANGE, KC_NWIN, KC_BWIN, KC_WSS, PAREN, EMAIL_1, EMAIL_2, EMAIL_3, UNI_NUM, L_RESET, KC_RNWIN, NAME, BIRTH, P_SHIFT, KC_MCWIN };
-
-
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT_LAYER] = LAYOUT(
-        _______,_______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        _______,KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,
-        KC_ESC, KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,
-        _______,KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_LSFT,
-                                LALT_T(KC_LNG2),MO(CL), KC_SPC, KC_LWIN,KC_LWIN,
+        XXXXXXX,XXXXXXX,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
+        XXXXXXX,KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   XXXXXXX,
+        KC_ESC, KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   XXXXXXX,
+        XXXXXXX,KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   MO(GL), _______,
+                                LALT_T(KC_LNG2),MO(CL), KC_SPC, KC_LWIN,KC_LSFT,
+
                 _______,KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,_______,_______,
                 KC_Y,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,_______,
                 KC_H,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,
-        KC_MINS,KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLS,_______,
-        KC_UNDS,KC_RGUI,KC_UNDS,MO(ML),RALT_T(KC_LNG1)
+        KC_MINS,KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_EQL, _______,
+        KC_MINS,KC_UNDS,KC_UNDS,MO(ML),RALT_T(KC_LNG1)
     ),
     [CONTROL_LAYER] = LAYOUT(
-        _______,_______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
-        _______,_______,_______,_______,_______,_______,_______,_______,
-        _______,_______,_______,KC_DEL, KC_DOWN,KC_ENT, _______,_______,
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,
+        XXXXXXX,XXXXXXX,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
+        XXXXXXX,_______,_______,_______,_______,_______,_______,XXXXXXX,
+        _______,_______,_______,KC_DEL, KC_DOWN,KC_ENT, _______,XXXXXXX,
+        XXXXXXX,_______,_______,_______,_______,_______,_______,XXXXXXX,_______,
                                 _______,        _______,_______,_______,_______,
 
                 _______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, _______,_______,
-                KC_Y,   KC_Y,   KC_HOME,KC_UP,  KC_END, KC_GRV, PAREN,  _______,
-                KC_H,   KC_H,   KC_LEFT,KC_DOWN,KC_RGHT,KC_ENT, KC_EQL, KC_CAPS,
-        KC_MINS,KC_B,   KC_BSPC,KC_DEL, KC_PGUP,KC_PGDN,KC_APP ,KC_APP, _______,
+                QK_BOOT,KC_PGUP,KC_HOME,KC_UP,  KC_END, KC_GRV, PAREN,  _______,
+              C(KC_TAB),KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,KC_ENT, KC_EQL, KC_F12,
+   KC_MINS,C(S(KC_TAB)),KC_BSPC,KC_DEL, _______,KC_BSLS,KC_APP ,KC_APP, _______,
         KC_UNDS,KC_UNDS,KC_NWIN,MO(ML),RALT_T(KC_LNG1)
     ),
     [MACHINE_CTRL_LAYER] = LAYOUT(
-        _______,_______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
         _______,_______,_______,_______,_______,_______,_______,_______,
-        _______,_______,_______,KC_DEL, KC_DOWN,KC_ENT, _______,_______,
+        _______,_______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,
                                         _______,_______,_______,_______,_______,
 
-                _______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, _______,_______,
-                KC_Y,   KC_Y,   KC_HOME,KC_UP,  KC_END, KC_GRV, PAREN,  _______,
-                KC_H,   KC_H,   KC_LEFT,KC_DOWN,KC_RGHT,KC_ENT, KC_EQL, KC_CAPS,
-        KC_MINS,KC_B,   KC_BSPC,KC_DEL, KC_PGUP,KC_PGDN,KC_APP ,KC_APP, _______,
-        KC_UNDS,KC_UNDS,KC_NWIN,MO(ML),RALT_T(KC_LNG1)
+                _______,_______,_______,_______,_______,_______,_______,_______,
+                _______,_______,_______,_______,_______,_______,_______,_______,
+                _______,_______,DF(GL), _______,_______,_______,_______,QK_BOOT,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______
     ),
+    [GAMING_LAYER] = LAYOUT(
+        XXXXXXX,XXXXXXX,KC_0,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,
+        XXXXXXX,KC_6,   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   XXXXXXX,
+        KC_9,   KC_ESC, KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   XXXXXXX,
+        XXXXXXX,KC_7,   KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   XXXXXXX,KC_M,
+                                KC_LALT,KC_G,   KC_SPC, KC_T,   KC_B,
+
+                _______,KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,_______,_______,
+                KC_Y,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,_______,
+                KC_H,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,
+        KC_MINS,KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_EQL, _______,
+        KC_MINS,KC_UNDS,KC_UNDS,DF(DL),DF(DL)
+    )
 };
 
 // clang-format on
-
-// send string ////////////////////////////////////////////////////////////////
-
-typedef struct {
-    int   keycode;
-    char *str;
-} SEND_STRING;
-
-bool send_text(uint16_t keycode, keyrecord_t *record) {
-    SEND_STRING send_strings[] = {{EMAIL_1, "mymail"}, {EMAIL_2, "mymail2"}, {EMAIL_3, "mymail3"}, {UNI_NUM, "uni num"}, {BIRTH, "1111 11 11"}};
-    for (int i = 0; i < sizeof(send_strings) / sizeof(send_strings[0]); i++) {
-        if (keycode != send_strings[i].keycode) continue;
-
-        if (record->event.pressed) send_string(send_strings[i].str);
-        return true;
-    }
-    return false;
-}
-
-// send string ////////////////////////////////////////////////////////////////
-
 // send command ///////////////////////////////////////////////////////////////
 void register_code(uint8_t code);
 void unregister_code(uint8_t code);
-
-void launch_snipping_tool(void) {
-    register_code(KC_LCTL);
-    register_code(KC_LWIN);
-    register_code(KC_S);
-    unregister_code(KC_LCTL);
-    unregister_code(KC_LWIN);
-    unregister_code(KC_S);
-}
 
 void reset_layer(void) {
     for (int i = 0; i < TOTAL_LAYERS; i++) {
@@ -134,7 +117,6 @@ typedef struct s_send_command {
  */
 bool send_command(uint16_t keycode, keyrecord_t *record) {
     SEND_COMMAND_PAIR send_commands[] = {
-        {KC_WSS, launch_snipping_tool},
         {L_RESET, reset_layer},
     };
 
@@ -225,7 +207,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
 
-    if (send_text(keycode, record)) return false;
     if (send_command(keycode, record)) return false;
 
     if ((shift_state == HOLD || shift_state == HOLD_PULSED_KEY) && record->event.pressed) {
@@ -265,85 +246,29 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 }
 
 void keyboard_post_init_user(void) {
-    debug_enable = true;
-    debug_matrix = true;
+    // debug_enable = true;
+    // debug_matrix = true;
 
-    wait_ms(500);
-    os_variant_t host = detected_host_os();
-    if (host == OS_MACOS || host == OS_IOS) {
-        // keymap_config.swap_lctl_lgui = true;
-        is_apple = true;
-        default_layer_set(1UL << MAC_DEFAULT_LAYER);
-        layer_move(MAC_DEFAULT_LAYER);
-    }
-    is_called_keyboard_post_init_user = detected_host_os();
+    // wait_ms(500);
+    // os_variant_t host = detected_host_os();
+    // if (host == OS_MACOS || host == OS_IOS) {
+    //     // keymap_config.swap_lctl_lgui = true;
+    //     is_apple = true;
+    //     default_layer_set(1UL << MAC_DEFAULT_LAYER);
+    //     layer_move(MAC_DEFAULT_LAYER);
+    // }
+    // is_called_keyboard_post_init_user = detected_host_os();
+    rgblight_layers=rgb_layers;
 }
 
-#define mac_layer (1 << MAC_DEFAULT_LAYER | 1 << MAC_CTL_LAYER | 1 << MAC_MACHINE_CTL_LAYER)
+// #define mac_layer (1 << MAC_DEFAULT_LAYER | 1 << MAC_CTL_LAYER | 1 << MAC_MACHINE_CTL_LAYER)
 
 const key_override_t ctrl_z_to_ctrl_u   = ko_make_basic(MOD_MASK_CTRL, KC_U, C(KC_Z));
 const key_override_t maximize           = ko_make_basic(MOD_MASK_GUI, KC_F, G(KC_UP));
-const key_override_t mac_cmd_z_to_cmd_u = ko_make_with_layers_negmods_and_options(MOD_MASK_CTRL, KC_U, LCTL(KC_Z), mac_layer, 0, ko_option_no_reregister_trigger);
-
-/*
-next word				ctrl+right		alt+right
-pre word				ctrl+left		alt+left
-line start				home			ctrl+a
-line end				end				ctrl+e
-file start				ctrl+home		ctrl+up
-file end				ctrl+end		ctrl+down
-delete to word start	ctrl+backspace	alt+backspace
-delete to word end		ctrl+delete		alt+delete
-move to pre page		alt+left		cmd+left
-move to y page			alt+right		cmd+right
-*/
-
-// move_word
-const key_override_t mac_move_left_word = ko_make_with_layers_negmods_and_options(MOD_MASK_GUI, KC_LEFT, LALT(KC_LEFT), mac_layer, 0, ko_option_no_reregister_trigger);
-
-const key_override_t mac_move_right_word = ko_make_with_layers_negmods_and_options(MOD_MASK_GUI, KC_RIGHT, LALT(KC_RIGHT), mac_layer, 0, ko_option_no_reregister_trigger);
-
-// select_word
-const key_override_t mac_select_left_word = ko_make_with_layers_negmods_and_options(MOD_MASK_SG, KC_LEFT, LAG(KC_LEFT), mac_layer, 0, ko_option_no_reregister_trigger);
-
-const key_override_t mac_select_right_word = ko_make_with_layers_negmods_and_options(MOD_MASK_SG, KC_RIGHT, LAG(KC_RIGHT), mac_layer, 0, ko_option_no_reregister_trigger);
-
-// move_line
-const key_override_t mac_move_line_start = ko_make_with_layers_negmods_and_options(0, KC_HOME, LCTL(KC_A), mac_layer, 0, ko_option_no_reregister_trigger);
-
-const key_override_t mac_move_line_end = ko_make_with_layers_negmods_and_options(0, KC_END, LCTL(KC_E), mac_layer, 0, ko_option_no_reregister_trigger);
-
-// delete
-const key_override_t mac_delete_to_word_start = ko_make_with_layers_negmods_and_options(MOD_MASK_GUI, KC_BACKSPACE, LALT(KC_BACKSPACE), mac_layer, 0, ko_option_no_reregister_trigger);
-
-const key_override_t mac_delete_to_word_end = ko_make_with_layers_negmods_and_options(MOD_MASK_GUI, KC_DELETE, LALT(KC_DELETE), mac_layer, 0, ko_option_no_reregister_trigger);
-
-// to file start/end
-const key_override_t mac_move_to_file_start = ko_make_with_layers_negmods_and_options(MOD_MASK_GUI, KC_HOME, LCTL(KC_UP), mac_layer, 0, ko_option_no_reregister_trigger);
-
-const key_override_t mac_move_to_file_end = ko_make_with_layers_negmods_and_options(MOD_MASK_GUI, KC_END, LCTL(KC_DOWN), mac_layer, 0, ko_option_no_reregister_trigger);
-
-// move page
-const key_override_t mac_move_pre_page = ko_make_with_layers_negmods_and_options(MOD_MASK_ALT, KC_LEFT, LGUI(KC_LEFT), mac_layer, 0, ko_option_no_reregister_trigger);
-
-const key_override_t mac_move_next_page = ko_make_with_layers_negmods_and_options(MOD_MASK_ALT, KC_RIGHT, LGUI(KC_RIGHT), mac_layer, 0, ko_option_no_reregister_trigger);
 
 // This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = (const key_override_t *[]){
     &maximize,
     &ctrl_z_to_ctrl_u,         //
-    &mac_cmd_z_to_cmd_u,       //
-    &mac_move_left_word,       //
-    &mac_move_right_word,      //
-    &mac_select_left_word,     //
-    &mac_select_right_word,    //
-    &mac_move_line_start,      //
-    &mac_move_line_end,        //
-    &mac_delete_to_word_start, //
-    &mac_delete_to_word_end,   //
-    // &mac_move_to_file_start,   //
-    // &mac_move_to_file_end,     //
-    &mac_move_pre_page,  //
-    &mac_move_next_page, //
     NULL,
 };
